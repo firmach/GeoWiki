578d9b85270f37f34fc54a222216d09091a37507
Roman Churkin <roman.churkin@teampicnic.com>
2023-05-31 21:25:03 +0200

    Initialise WikiLocations app — places deeplinks test app

---
+++ WikiLocations/WikiLocations/SceneDelegate.swift
@@ -0,0 +1,40 @@
+//
+//  SceneDelegate.swift
+//  WikiLocations
+//
+//  Created by Roman Churkin on 31/05/2023.
+//
+
+import UIKit
+
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+
+    var window: UIWindow?
+
+
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+
+        self.window = UIWindow(windowScene: windowScene)
+
+        let viewController = ViewController()
+
+        let navigationController = UINavigationController(rootViewController: viewController)
+        navigationController.navigationBar.prefersLargeTitles = true
+
+        window?.rootViewController = navigationController
+        window?.makeKeyAndVisible()
+    }
+
+    func sceneDidDisconnect(_ scene: UIScene) { }
+
+    func sceneDidBecomeActive(_ scene: UIScene) { }
+
+    func sceneWillResignActive(_ scene: UIScene) { }
+
+    func sceneWillEnterForeground(_ scene: UIScene) { }
+
+    func sceneDidEnterBackground(_ scene: UIScene) { }
+
+}
